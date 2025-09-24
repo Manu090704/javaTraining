b92ec2a1998e5fd86d744a061b526a0a16b39fac
Manuel Bajos Rivera <149975766+Manu090704@users.noreply.github.com>
2025-09-24 15:01:37 -0600

    Update hilos.java

--- Hilos/hilos.java
+++ Hilos/hilos.java
@@ -1,5 +1,7 @@
 package Hilos;
 
+//concurrencia: hacer varias tareas al mismo tiempo pero intercalando.
+//paralelismo: hacer varias tareas al mismo tiempo pero al mismo tiempo.
 public class hilos implements Runnable{
     @Override
     public void run() {
