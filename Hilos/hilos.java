0382025bc803e49a6138c9cbc958588962b2ae18
Manuel Bajos Rivera <149975766+Manu090704@users.noreply.github.com>
2025-09-24 15:00:34 -0600

    Update hilos.java

--- Hilos/hilos.java
+++ Hilos/hilos.java
@@ -4,6 +4,13 @@ public class hilos implements Runnable{
     @Override
     public void run() {
         System.out.println("Concurrencia: ");
-        System.out.println("Hola desde un hilo");
+        System.out.println("Hola desde un hilo por interfaz");
+    }
+}
+
+public class extends Threads {
+    @Override
+    public void run(){
+    System.out.println("Hola desde un hilo por clase");
     }
 }
